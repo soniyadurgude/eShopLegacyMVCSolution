5d26cd830b6f5f101f8328eeb730af9f285d6a0b
Soniya Durgude <soniya_durgude@persistent.com>
2025-03-25 13:09:58 +0530

    Added new target MVC Core application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EShopCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
